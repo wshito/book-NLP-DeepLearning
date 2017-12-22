70a0667bf079781e027eea290f649fce99a0e127
Wataru Shito <wataru007jp@gmail.com>
2017-12-22 23:55:57 +0900

    refactored

--- ch02/ch02.lisp
+++ ch02/ch02.lisp
@@ -5,6 +5,17 @@
 (in-package :nlp-dl.ch02)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Utility
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defmacro write-char> (ch stream) ; for stream chaining
+  "Returns the output stream for stream chaining as
+   (write-char> ch3 (write-char> ch2 (write-char> ch1 (make-string-output-stream))))"
+  (let ((s (gensym)))
+    `(let ((,s ,stream))
+       (progn (write-char ,ch ,s)
+	      ,s))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; 2.2.1 文字の処理
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -20,7 +31,7 @@
 ;; 文字列からn-gramのリストを作成して返す．
 ;; non-asciiキーワードがtならAscii文字を取り除く．
 ;; stringキーワードがtなら結果を文字列のリストにして返す．
-(defun make-n-gram (str n &key (non-ascii t) (string nil))
+(defun make-n-gram-from-str (str n &key (non-ascii t) (string nil))
   "N-gramを生成する．"
   (when non-ascii (setf str (extract-nonascii str)))
   ;; 1-gram
@@ -63,64 +74,69 @@
 ;; unicodeから文字
 ;; (format t "~:c" (code-char #x9F02))
 
-(defun chartype (ch)
+(defmacro chartype (ch)
   "1文字分のunicodeポイントコードを取り，文字種のシンボルを返す"
-  (let ((unicode (char-code ch)))
-    (cond
-      ((and (< #x002F unicode) (< unicode #x003A)) 'number)
-      ((and (< #x0040 unicode) (< unicode #x005B)) 'alphabet) ; 大文字
-      ((and (< #x0060 unicode) (< unicode #x007B)) 'alphabet) ; 小文字
-      ((and (< #x3040 unicode) (< unicode #x30A0)) 'hiragana)
-      ((and (< #x309F unicode) (< unicode #x30FF)) 'katakana)
-      ((and (<= #x4E00 unicode) (<= unicode #x9FA0)) 'kanji)
-      ((and (< #xFF65 unicode ) (< unicode #xFF9F)) 'hankaku-kana)
-      (t 'punctuation))))
+  (let ((c (gensym)))
+    `(let* ((,c ,ch)
+	    (unicode (char-code ,c)))
+       (cond
+	 ((and (< #x002F unicode) (< unicode #x003A)) 'number)
+	 ((and (< #x0040 unicode) (< unicode #x005B)) 'alphabet) ; 大文字
+	 ((and (< #x0060 unicode) (< unicode #x007B)) 'alphabet) ; 小文字
+	 ((and (< #x3040 unicode) (< unicode #x30A0)) 'hiragana)
+	 ((and (< #x309F unicode) (< unicode #x30FF)) 'katakana)
+	 ((and (<= #x4E00 unicode) (<= unicode #x9FA0)) 'kanji)
+	 ((and (< #xFF65 unicode ) (< unicode #xFF9F)) 'hankaku-kana)
+	 (t 'punctuation)))))
 
 (defun split-chartype (in &key (string nil))
-  "inputストリームを取り文字種で分割したリストを返す．`:string`が`t`なら文字列の，`nil`ならシンボルのリストを返す．"
-  (macrolet ((make-result (stream result)
-	       `(reverse
-		 (cons (if string (get-output-stream-string ,stream)
-			   (intern (get-output-stream-string ,stream)))
-		       ,result))))
+  "Inputストリームを取り文字種で分割したリストを返す．`:string`が`t`なら文字列の，`nil`ならシンボルのリストを返す．"
+  (macrolet ((%make-result (stream result)
+	       `(cons (if string (get-output-stream-string ,stream)
+			  (intern (get-output-stream-string ,stream)))
+		      ,result)))
     (labels ((%split (in type nextch word res) ; type for current char
-	     (if (null nextch) (make-result word res)
-		 (let ((next-type (chartype nextch)))
-		   ; (format t "ch=~:c~%" nextch) ; for debug
-		   (if (eql type next-type)
-		       (progn ; type == next-type
-			 (write-char nextch word)
-			 (%split in type (read-char in nil) word res))
-		       (progn ; type != next-type
-			 ;; skip punctuation
-			 (loop while (eql next-type 'punctuation)
-			    do
-			      (setf nextch (read-char in nil))
-			      (when (null nextch) (return))
-			      (setf next-type (chartype nextch)))
-			 (if (null nextch) 
-			     ;; end of the stream while looking for
-			     ;; non-punctuation
-			     (make-result word res)
-			     ;; new word found
-			     (let ((newword (make-string-output-stream)))
-			       (write-char nextch newword)
+	       (if (null nextch) (reverse (%make-result word res))
+		   (let ((next-type (chartype nextch)))
+		     ;; (format t "ch=~:c~%" nextch) ; for debug
+		     (if (eql type next-type)
+			 (%split in type (read-char in nil)
+				 (write-char> nextch word) res)
+			 (progn ; type != next-type
+			   ;; skip punctuation
+			   (loop while (eql next-type 'punctuation)
+			      do
+				(setf nextch (read-char in nil))
+				(when (null nextch) (return))
+				(setf next-type (chartype nextch)))
+			   (if (null nextch) 
+			       ;; end of the stream while looking for
+			       ;; non-punctuation
+			       (reverse (%make-result word res))
+			       ;; new word found
 			       (%split in next-type (read-char in nil)
-				       newword
-				       (cons (if string
-						 (get-output-stream-string word)
-						 (intern (get-output-stream-string word)))
-					     res))))))))))
-    ;; main routine
-    (let ((ch (read-char in nil)))
-      (if (null ch) nil
-	  (progn ; skip the punctuation first
-	    (loop while (eql (chartype ch) 'punctuation)
-	       do (setf ch (read-char in nil)))
-	    ; (format t "1st ch=~:c~%" ch) ; for debug
-	    (let ((out (make-string-output-stream)))
-	      (write-char ch out)
-	      (%split in (chartype ch) (read-char in nil) out nil))))))))
+				       (write-char> nextch
+						    (make-string-output-stream))
+				       (%make-result word res)))))))))
+      ;; main routine
+      (let ((ch (read-char in nil)))
+	(if (null ch) nil
+	    (progn ; skip the punctuation first
+	      (loop while (eql (chartype ch) 'punctuation)
+		 do (setf ch (read-char in nil)))
+	      ;; (format t "1st ch=~:c~%" ch) ; for debug
+	      (%split in (chartype ch) (read-char in nil)
+		      (write-char> ch (make-string-output-stream))
+		      nil)))))))
 
 
+(defun make-n-gram (n lst)
+  "リストからN-Gramを作成して返す．"
+  (if (> 2 n) lst
+      (labels ((%nhead (n lst res)
+		 (if (null lst) (reverse res)
+		     (if (= n 1) (reverse (cons (car lst) res))
+			 (%nhead (1- n) (cdr lst) (cons (car lst) res))))))
+	(loop for x on lst
+	     collect (%nhead n x nil)))))
 
