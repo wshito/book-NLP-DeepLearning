06fae2421d1cd96d386007798b160c55977232af
Wataru Shito <wataru007jp@gmail.com>
2017-12-22 23:56:10 +0900

    char type n-gram

--- ch02/ch02-sample.lisp
+++ ch02/ch02-sample.lisp
@@ -6,11 +6,18 @@
 
 (defparameter *すもも* "すもももももももものうち")
 
-(make-n-gram *sample* 2)
-(make-n-gram *sample* 3)
-(make-n-gram *sample* 4)
 
-(make-bag (make-n-gram *sample* 2))
-(make-bag (make-n-gram *すもも* 3))
+(make-n-gram-from-str *sample* 2)
+(make-n-gram-from-str *sample* 3)
+(make-n-gram-from-str *sample* 4)
 
+(make-bag (make-n-gram-from-str *sample* 2))
+(make-bag (make-n-gram-from-str *すもも* 3))
 
+;;; 文字種で分割
+(split-chartype (make-string-input-stream *sample*))
+(split-chartype (make-string-input-stream *すもも*))
+
+;;; 文字種からN-Gramを作成
+(make-n-gram 2 (split-chartype (make-string-input-stream *sample*)))
+(make-n-gram 3 (split-chartype (make-string-input-stream *sample*)))
