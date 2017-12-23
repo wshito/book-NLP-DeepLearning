e45b25fe947971371b10ad25043396ac3abd4668
Wataru Shito <wataru007jp@gmail.com>
2017-12-23 18:04:14 +0900

    using ch02 package

--- ch02/ch02-sample.lisp
+++ ch02/ch02-sample.lisp
@@ -1,6 +1,6 @@
 (load "ch02.lisp")
 
-(use-package :nlp-dl.ch02)
+(in-package :nlp-dl.ch02)
 
 (defparameter *sample* "自然言語処理(natural language processing)のうちで基本的かつ実用的な技術は，自然言語テキストの入力と編集を支援する技術でしょう．")
 
